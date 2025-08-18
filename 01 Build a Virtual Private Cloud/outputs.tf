9d86dec1709c8344aa68b4ee7ed7443048b68421
Robert Amoah <mr_robertamoah@yahoo.com>
2025-08-18 13:08:38 +0000

    Update outputs configuration for VPC and subnets

--- 01 Build a Virtual Private Cloud/outputs.tf
+++ 01 Build a Virtual Private Cloud/outputs.tf
@@ -5,3 +5,11 @@ output "available_zones" {
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "aws_internet_gateway_id" {
+  value = aws_internet_gateway.gw.id
+}
