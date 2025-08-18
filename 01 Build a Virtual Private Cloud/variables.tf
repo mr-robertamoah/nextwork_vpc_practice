1ca3ef7bc1df7d42089d93267c9f05d77b715b06
Robert Amoah <mr_robertamoah@yahoo.com>
2025-08-18 12:56:32 +0000

    Add variables definition for VPC setup

---
+++ 01 Build a Virtual Private Cloud/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.2.0.0/16"
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the VPC"
+  type        = map(string)
+  default = {
+    Name        = "nextwork_iac"
+    Environment = "development"
+  }
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  default     = "10.2.1.0/24"
+}
+
