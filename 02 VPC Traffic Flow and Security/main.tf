4befd2c31fb2c50ef9228299381153915ecb1558
Robert Amoah <mr_robertamoah@yahoo.com>
2025-08-21 13:51:40 +0000

    Adjust main configuration for traffic flow/security

--- 02 VPC Traffic Flow and Security/main.tf
+++ 02 VPC Traffic Flow and Security/main.tf
@@ -51,6 +51,47 @@ resource "aws_network_acl" "public_subnet_nacl" {
   })
 }
 
+# inbound rules for the public subnet
+resource "aws_network_acl_rule" "public_subnet_nacl_ingress_http" {
+  network_acl_id = aws_network_acl.public_subnet_nacl.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
+resource "aws_network_acl_rule" "public_subnet_nacl_ingress_https" {
+  network_acl_id = aws_network_acl.public_subnet_nacl.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
+# outbound rules for the public subnet
+resource "aws_network_acl_rule" "public_subnet_nacl_egress" {
+  network_acl_id = aws_network_acl.public_subnet_nacl.id
+  rule_number    = 200
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = -1
+  to_port        = -1
+}
+
+# associate public nacl to public subnet
+resource "aws_network_acl_association" "public_subnet" {
+  subnet_id     = aws_subnet.public.id
+  network_acl_id = aws_network_acl.public_subnet_nacl.id
+}
+
 # create security group for public subnet
 resource "aws_security_group" "public_subnet_sg" {
   vpc_id = aws_vpc.main.id
