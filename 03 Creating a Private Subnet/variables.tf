ea60d47cb061ad6a50552c5c6622e8197b96ca4f
Robert Amoah <mr_robertamoah@yahoo.com>
2025-08-21 13:46:56 +0000

    Add variables for private subnet configuration

---
+++ 03 Creating a Private Subnet/variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.2.0.0/16"
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the VPC"
+  type        = map(string)
+  default = {
+    Name        = "nextwork_iac"
+    Environment = "development"
+  }
+}
+
+variable "subnet_cidrs" {
+  description = "The CIDR block for the public subnet"
+  type        = list(string)
+  default     = ["10.2.1.0/24", "10.2.2.0/24"]
+}
+
